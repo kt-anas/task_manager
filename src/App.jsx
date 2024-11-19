4739274ae6e62e0b81d0e468e93e26c984cf49e8
anas <ktmanas2020@gmail.com>
2024-11-19 20:50:17 +0530

    task

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,40 @@
- 
- import react from 'react'
-import Center from './components/Center'
-import Header from './components/Header'
-
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import EmptyBoard from './components/EmptyBoard';
+import boardsSlice from "./redux/boardsSlice";
 
 function App() {
-  
-
+  const [isBoardModalOpen, setIsBoardModalOpen] = useState(false);
+  const dispatch = useDispatch();
+  const boards = useSelector((state) => state.boards);
+  const activeBoard = boards.find((board) => board.isActive);
+  if (!activeBoard && boards.length > 0)
+    dispatch(boardsSlice.actions.setBoardActive({ index: 0 }));
   return (
-    <div>
+    <div className=" overflow-hidden  overflow-x-scroll">
+      <>
+        {boards.length > 0 ?
+        <>
+        <Header
+          setIsBoardModalOpen={setIsBoardModalOpen}
+          isBoardModalOpen={isBoardModalOpen}
+        />
+        <Home
+          setIsBoardModalOpen={setIsBoardModalOpen}
+          isBoardModalOpen={isBoardModalOpen}
+        />
+        </>
+        :
+        <>
+          <EmptyBoard type='add'/>
+        </>
+      }
         
-    <Header/>   
-
-    <Center/>
-      
+      </>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/hooks/useDarkMode.jsx
+++ src/hooks/useDarkMode.jsx
@@ -1,17 +1,21 @@
 import React, { useEffect, useState } from 'react'
 
 function useDarkMode() {
-  const [theme, setTheme] = useState(localStorage.theme);
-  const colorTheme = theme === 'dark' ? 'light' : 'dark';
+    
+  const [theme, setTheme] = useState(localStorage.theme)
+  const colorTheme = theme === "dark" ? "light" : "dark";
+
   useEffect(() => {
-      const root = window.document.documentElement;
-      root.classList.remove(colorTheme);
-      root.classList.add(theme);
-      localStorage.setItem('theme', theme); 
 
-  }, [theme, colorTheme]);
+    const root = window.document.documentElement;
+    root.classList.remove(colorTheme);
+    root.classList.add(theme);
+    localStorage.setItem('theme', theme);
 
-  return [colorTheme, setTheme];
+}, [theme, colorTheme]);
+
+return [colorTheme, setTheme]
+
 }
 
 export default useDarkMode
