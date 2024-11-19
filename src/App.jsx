55658f3348693401367ef960273bb43b51365ca7
anas <ktmanas2020@gmail.com>
2024-11-19 10:42:05 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+ 
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1 className="text-red-800 font-bold underline">
+    Hello world!
+     </h1>
+      
+    </>
+  )
+}
+
+export default App
